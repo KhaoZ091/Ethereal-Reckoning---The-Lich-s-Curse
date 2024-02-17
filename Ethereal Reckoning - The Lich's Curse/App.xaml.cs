d549b23c3282ea8deb7e4d72e7be3ecfcf0c6b42
cyrus <cyrus@DESKTOP-BMHD6JG>
2024-02-17 12:46:21 +0800

    Add project files.

---
+++ Ethereal Reckoning - The Lich's Curse/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ethereal_Reckoning___The_Lich_s_Curse
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
